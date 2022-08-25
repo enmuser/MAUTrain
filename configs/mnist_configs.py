b1473eb29a1027bee04134f9d5406d859bf96a47
朱洪昌 <1282801732@qq.com>
2022-08-25 22:22:17 +0800

    Update mnist_configs.py

--- configs/mnist_configs.py
+++ configs/mnist_configs.py
@@ -7,9 +7,9 @@ import argparse
 def configs():
     parser = argparse.ArgumentParser(description='MAU_test')
 
-    parser.add_argument('--data_train_path', type=str, default='/kaggle/working/MAUTrain/data/mnist_dataset/')
-    parser.add_argument('--data_val_path', type=str, default='/kaggle/working/MAUTrain/data/mnist_dataset/')
-    parser.add_argument('--data_test_path', type=str, default='/kaggle/working/MAUTrain/data/mnist_dataset/')
+    parser.add_argument('--data_train_path', type=str, default='/kaggle/working/MAUTrain/data/')
+    parser.add_argument('--data_val_path', type=str, default='/kaggle/working/MAUTrain/data/')
+    parser.add_argument('--data_test_path', type=str, default='/kaggle/working/MAUTrain/data/')
     parser.add_argument('--input_length', type=int, default=10)
     parser.add_argument('--real_length', type=int, default=20)
     parser.add_argument('--total_length', type=int, default=20)
